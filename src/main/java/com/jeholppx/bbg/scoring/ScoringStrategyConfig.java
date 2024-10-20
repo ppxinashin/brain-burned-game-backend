55b098c352a71a847e97bfa1bcfdd97e67e67e88
ppxinashin <billzhang.623@gmail.com>
2024-10-20 16:26:23 +0800

    modify 注释优化

--- src/main/java/com/jeholppx/bbg/scoring/ScoringStrategyConfig.java
+++ src/main/java/com/jeholppx/bbg/scoring/ScoringStrategyConfig.java
@@ -7,14 +7,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@Component
-
 /**
  * @author <a href="https://www.jehol-ppx.com">热河fen青</a>
  * @date 2024/10/16 15:59
  */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
 public @interface ScoringStrategyConfig {
 
     int appType();
